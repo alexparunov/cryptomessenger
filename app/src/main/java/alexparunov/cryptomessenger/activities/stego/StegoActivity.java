c9f7fe6e2f9f9eb55869e2c656d9f3e88e953ddc
Alexander Parunov <alexander.parunov@gmail.com>
2017-02-09 20:55:49 +0200

    Added StegoActivity

---
+++ app/src/main/java/alexparunov/cryptomessenger/activities/stego/StegoActivity.java
@@ -0,0 +1,15 @@
+package alexparunov.cryptomessenger.activities.stego;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import alexparunov.cryptomessenger.R;
+
+public class StegoActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_stego);
+  }
+}
