12d0af5be36233300d9c1f2c00302e57d4028196
Alexander Parunov <alexander.parunov@gmail.com>
2017-02-09 21:55:16 +0200

    Fill imageView in StegoActivity

--- app/src/main/java/alexparunov/cryptomessenger/activities/stego/StegoActivity.java
+++ app/src/main/java/alexparunov/cryptomessenger/activities/stego/StegoActivity.java
@@ -7,13 +7,27 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
 import alexparunov.cryptomessenger.R;
 import alexparunov.cryptomessenger.utils.Constants;
 import alexparunov.cryptomessenger.utils.HelperMethods;
 import alexparunov.cryptomessenger.utils.StandardMethods;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class StegoActivity extends AppCompatActivity implements StegoView {
+
+
+  @BindView(R.id.ivStegoImage)
+  ImageView ivStegoImage;
+
+  @OnClick({R.id.bStegoSave, R.id.bStegoShare})
+  public void onClick(View view) {
+    //(TODO) Implement onClick
+  }
 
 
   private ProgressDialog progressDialog;
@@ -24,11 +38,14 @@ public class StegoActivity extends AppCompatActivity implements StegoView {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_stego);
 
+    ButterKnife.bind(this);
+
     Intent intent = getIntent();
     if (intent != null) {
       byte[] byteArrayExtra = intent.getByteArrayExtra(Constants.EXTRA_STEGO_IMAGE_ARRAY);
       stegoImage = HelperMethods.byteArrayToBitmap(byteArrayExtra);
       setStegoImage(stegoImage);
+      ivStegoImage.setImageBitmap(stegoImage);
     }
 
     initToolbar();
