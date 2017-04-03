c594ce7d16841aa6fbaffa69c3ad66c3b7d532ef
Alexander Parunov <alexander.parunov@gmail.com>
2017-04-03 17:39:12 +0300

    Commented out stego package

--- app/src/main/java/alexparunov/cryptomessenger/activities/stego/StegoPresenter.java
+++ app/src/main/java/alexparunov/cryptomessenger/activities/stego/StegoPresenter.java
@@ -6,6 +6,11 @@ package alexparunov.cryptomessenger.activities.stego;
 
 interface StegoPresenter {
 
+  /**
+   * Saves stego image in background by sending broadCast
+   * @param stegoPath is path to stego image which should be saved
+   * @return true if image is successfully saved.
+   */
   boolean saveStegoImage(String stegoPath);
 
 }
